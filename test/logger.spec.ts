74c4302ac0d6aa8b0246762bfba3dc92104a6a0c
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2022-05-31 00:19:32 +0300

    update logger tests

--- test/logger.spec.ts
+++ test/logger.spec.ts
@@ -1,13 +1,12 @@
 ﻿import { log, injectLogger } from "../src";
 
-describe("logger", () => {
-    it("can log without injecting", () => {
-        expect(() => log("")).not.toThrow();
-    });
-    it("can inject custom logger", () => {
-        let message = "";
-        injectLogger(msg => message = msg);
-        log("foo");
-        expect(message).toEqual("foo");
-    });
+test("can log without injecting", () => {
+    expect(() => log("")).not.toThrow();
+});
+
+test("can inject custom logger", () => {
+    let message = "";
+    injectLogger(msg => message = msg);
+    log("foo");
+    expect(message).toEqual("foo");
 });
