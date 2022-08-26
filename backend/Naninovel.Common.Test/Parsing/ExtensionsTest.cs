543e5ad66dfff86c4fd5d08740d23374d6dae9a5
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2022-08-26 19:33:43 +0300

    add get line type extension

---
+++ backend/Naninovel.Common.Test/Parsing/ExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace Naninovel.Parsing.Test;
+
+public class ExtensionsTest
+{
+    [Fact]
+    public void CanResolveLineType ()
+    {
+        Assert.Equal(LineType.Comment, new CommentLine("").GetLineType());
+        Assert.Equal(LineType.Label, new LabelLine("").GetLineType());
+        Assert.Equal(LineType.Command, new CommandLine(new("")).GetLineType());
+        Assert.Equal(LineType.Generic, new GenericLine(Array.Empty<IGenericContent>()).GetLineType());
+    }
+
+    [Fact]
+    public void WhenResolvingUnknownLineTypeThrows ()
+    {
+        Assert.Throws<Error>(() => Extensions.GetLineType(null));
+    }
+}
---
+++ backend/Naninovel.Common/Parsing/Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Naninovel.Parsing;
+
+/// <summary>
+/// Extensions for lexing and parsing types.
+/// </summary>
+public static class Extensions
+{
+    /// <summary>
+    /// Resolves <see cref="LineType"/> of the specified parsed line instance.
+    /// </summary>
+    public static LineType GetLineType (this IScriptLine line) => line switch {
+        CommentLine => LineType.Comment,
+        LabelLine => LineType.Label,
+        CommandLine => LineType.Command,
+        GenericLine => LineType.Generic,
+        _ => throw new Error($"Unknown line type: {line}")
+    };
+}
