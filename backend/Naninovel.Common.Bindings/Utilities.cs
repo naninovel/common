fe95a56c28155965e0775f8c21473fcb1d75beb6
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2022-09-01 18:41:40 +0300

    add try overloads

--- backend/Naninovel.Common.Bindings/Utilities.cs
+++ backend/Naninovel.Common.Bindings/Utilities.cs
@@ -2,7 +2,6 @@
 using System.Text.Json.Serialization;
 using DotNetJS;
 using Naninovel.Common.Bindings;
-using static Naninovel.Common.Bindings.JSLogger;
 
 [assembly: JSNamespace(Utilities.NamespacePattern, Utilities.NamespaceReplacement)]
 
@@ -24,13 +23,83 @@ public static class Utilities
     public static void Try (Action action)
     {
         try { action(); }
-        catch (Exception e) { LogError($"{action.GetType().Namespace} error: {e}"); }
+        catch (Exception e) { LogException(action, e); }
     }
 
-    public static T Try<T> (Func<T> func)
+    public static void Try<T1> (Action<T1> action, T1 arg1)
+    {
+        try { action(arg1); }
+        catch (Exception e) { LogException(action, e); }
+    }
+
+    public static void Try<T1, T2> (Action<T1, T2> action, T1 arg1, T2 arg2)
+    {
+        try { action(arg1, arg2); }
+        catch (Exception e) { LogException(action, e); }
+    }
+
+    public static void Try<T1, T2, T3> (Action<T1, T2, T3> action, T1 arg1, T2 arg2, T3 arg3)
+    {
+        try { action(arg1, arg2, arg3); }
+        catch (Exception e) { LogException(action, e); }
+    }
+
+    public static void Try<T1, T2, T3, T4> (Action<T1, T2, T3, T4> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+    {
+        try { action(arg1, arg2, arg3, arg4); }
+        catch (Exception e) { LogException(action, e); }
+    }
+
+    public static void Try<T1, T2, T3, T4, T5> (Action<T1, T2, T3, T4, T5> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+    {
+        try { action(arg1, arg2, arg3, arg4, arg5); }
+        catch (Exception e) { LogException(action, e); }
+    }
+
+    public static TResult Try<TResult> (Func<TResult> func)
     {
         try { return func(); }
-        catch (Exception e) { LogError($"{func.GetType().Namespace} error: {e}"); }
+        catch (Exception e) { LogException(func, e); }
         return default!;
+    }
+
+    public static TResult Try<T1, TResult> (Func<T1, TResult> func, T1 arg1)
+    {
+        try { return func(arg1); }
+        catch (Exception e) { LogException(func, e); }
+        return default!;
+    }
+
+    public static TResult Try<T1, T2, TResult> (Func<T1, T2, TResult> func, T1 arg1, T2 arg2)
+    {
+        try { return func(arg1, arg2); }
+        catch (Exception e) { LogException(func, e); }
+        return default!;
+    }
+
+    public static TResult Try<T1, T2, T3, TResult> (Func<T1, T2, T3, TResult> func, T1 arg1, T2 arg2, T3 arg3)
+    {
+        try { return func(arg1, arg2, arg3); }
+        catch (Exception e) { LogException(func, e); }
+        return default!;
+    }
+
+    public static TResult Try<T1, T2, T3, T4, TResult> (Func<T1, T2, T3, T4, TResult> func, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+    {
+        try { return func(arg1, arg2, arg3, arg4); }
+        catch (Exception e) { LogException(func, e); }
+        return default!;
+    }
+
+    public static TResult Try<T1, T2, T3, T4, T5, TResult> (Func<T1, T2, T3, T4, T5, TResult> func, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+    {
+        try { return func(arg1, arg2, arg3, arg4, arg5); }
+        catch (Exception e) { LogException(func, e); }
+        return default!;
+    }
+
+    private static void LogException (object func, Exception e)
+    {
+        JSLogger.LogError($"{func.GetType().Namespace} error: {e}");
     }
 }
