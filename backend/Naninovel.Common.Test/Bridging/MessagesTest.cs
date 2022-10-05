475f2983ef17cb640fb9e7596c6f7ddeff90f9b3
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2022-10-05 19:50:31 +0300

    cover messages

---
+++ backend/Naninovel.Common.Test/Bridging/MessagesTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Naninovel.Bridging.Test;
+
+public class MessagesTest
+{
+    [Fact] // TODO: Change to required when Unity allows and remove this test.
+    [SuppressMessage("ReSharper", "HeuristicUnreachableCode")]
+    public void MessagePropsAreNullByDefault ()
+    {
+        Assert.Null(new GotoRequest().PlaybackSpot);
+        Assert.Null(new PlaybackSpot().ScriptName);
+        Assert.Null(new PlaybackStatus().PlayedSpot);
+        Assert.Null(new UpdateMetadata().Metadata);
+        Assert.Null(new UpdatePlaybackStatus().PlaybackStatus);
+    }
+}
