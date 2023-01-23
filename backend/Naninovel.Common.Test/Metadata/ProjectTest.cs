14e94bee24135b0b95a807705e4dc02e7177d030
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-01-23 18:10:13 +0300

    add test

---
+++ backend/Naninovel.Common.Test/Metadata/ProjectTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Naninovel.Metadata.Test;
+
+public class ProjectTest
+{
+    [Fact] // TODO: Change to required when Unity allows and remove this test.
+    public void MessagePropsInitializedWithDefaults ()
+    {
+        Assert.Equal(default, new ValueContext().Type);
+        Assert.Equal(default, new ValueContext().SubType);
+    }
+}
