93c2f591bb94a0438604a5328c216e94e4f4424a
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-01-23 17:08:15 +0300

    add test

--- backend/Naninovel.Common.Test/Observing/NotifierTest.cs
+++ backend/Naninovel.Common.Test/Observing/NotifierTest.cs
@@ -48,4 +48,16 @@ public class NotifierTest
         var notifier = new ObserverNotifier<IMockObserver>(new[] { observer1.Object, observer2.Object });
         notifier.Notify(o => o.Handle(), o => o.Reverse());
     }
+
+    [Fact]
+    public async Task CanChangeNotifyOrderAsync ()
+    {
+        var notifyCounter = 0;
+        var observer1 = new Mock<IMockObserver>();
+        var observer2 = new Mock<IMockObserver>();
+        observer1.Setup(o => o.HandleAsync()).Callback(() => Assert.Equal(2, ++notifyCounter));
+        observer2.Setup(o => o.HandleAsync()).Callback(() => Assert.Equal(1, ++notifyCounter));
+        var notifier = new ObserverNotifier<IMockObserver>(new[] { observer1.Object, observer2.Object });
+        await notifier.NotifyAsync(o => o.HandleAsync(), o => o.Reverse());
+    }
 }
