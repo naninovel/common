c87097644a5bbd66c7e2d1545c3cfbc0b14e89cb
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-01-23 17:12:42 +0300

    add test

--- backend/Naninovel.Common.Test/Bridging/MessagesTest.cs
+++ backend/Naninovel.Common.Test/Bridging/MessagesTest.cs
@@ -7,11 +7,14 @@ public class MessagesTest
 {
     [Fact] // TODO: Change to required when Unity allows and remove this test.
     [SuppressMessage("ReSharper", "HeuristicUnreachableCode")]
-    public void MessagePropsAreNullByDefault ()
+    public void MessagePropsInitializedWithDefaults ()
     {
         Assert.Null(new GotoRequest().PlaybackSpot);
         Assert.Null(new PlaybackSpot().ScriptName);
+        Assert.Equal(0, new PlaybackSpot().LineIndex);
+        Assert.Equal(0, new PlaybackSpot().InlineIndex);
         Assert.Null(new PlaybackStatus().PlayedSpot);
+        Assert.False(new PlaybackStatus().Playing);
         Assert.Null(new UpdateMetadata().Metadata);
         Assert.Null(new UpdatePlaybackStatus().PlaybackStatus);
     }
