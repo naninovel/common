421d88a8b8da45a53b6d50a39735e3f17a57370a
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-02-16 17:06:30 +0300

    etc

--- backend/Naninovel.Common/Bridging/Finder/ServerInfo.cs
+++ backend/Naninovel.Common/Bridging/Finder/ServerInfo.cs
@@ -5,7 +5,7 @@ public class ServerInfo
     public string Name { get; }
     public int Port { get; }
 
-    internal ServerInfo (string name, int port)
+    public ServerInfo (string name, int port)
     {
         Name = name;
         Port = port;
