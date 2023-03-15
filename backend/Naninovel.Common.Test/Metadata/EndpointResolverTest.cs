3dd2eed0e82d3f14a4b69db0a998d09a067a3ce0
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-03-15 18:23:58 +0300

    add endpoint resolver

---
+++ backend/Naninovel.Common.Test/Metadata/EndpointResolverTest.cs
@@ -0,0 +1,83 @@
+﻿using Naninovel.Parsing;
+using Xunit;
+
+namespace Naninovel.Metadata.Test;
+
+public class EndpointResolverTest
+{
+    private readonly EndpointResolver resolver;
+    private readonly MetadataProvider meta = new();
+
+    public EndpointResolverTest ()
+    {
+        resolver = new(meta);
+    }
+
+    [Fact]
+    public void WhenUnknownCommandReturnsFalse ()
+    {
+        Assert.False(resolver.TryResolve(new("c"), out _, out _));
+    }
+
+    [Fact]
+    public void WhenUnknownParameterReturnsFalse ()
+    {
+        meta.Update(new() {
+            Commands = new[] { new Command { Id = "c" } }
+        });
+        Assert.False(resolver.TryResolve(new("c"), out _, out _));
+    }
+
+    [Fact]
+    public void WhenOtherParameterReturnsFalse ()
+    {
+        meta.Update(new() {
+            Commands = new[] { new Command { Id = "c", Parameters = new[] { new Parameter { Id = "p" } } } }
+        });
+        Assert.False(resolver.TryResolve(new("c", new[] { new Parsing.Parameter("p", new[] { new PlainText("v") }) }), out _, out _));
+    }
+
+    [Fact]
+    public void CanResolveScript ()
+    {
+        meta.Update(new() {
+            Commands = new[] { new Command { Id = "c", Parameters = new[] { CreateEndpointParameterMeta("p") } } }
+        });
+        Assert.True(resolver.TryResolve(new("c", new[] { new Parsing.Parameter("p", new[] { new PlainText("s") }) }), out var script, out _));
+        Assert.Equal("s", script);
+    }
+
+    [Fact]
+    public void CanResolveLabel ()
+    {
+        meta.Update(new() {
+            Commands = new[] { new Command { Id = "c", Parameters = new[] { CreateEndpointParameterMeta("p") } } }
+        });
+        Assert.True(resolver.TryResolve(new("c", new[] { new Parsing.Parameter("p", new[] { new PlainText(".l") }) }), out _, out var label));
+        Assert.Equal("l", label);
+    }
+
+    [Fact]
+    public void CanResolveScriptAndLabel ()
+    {
+        meta.Update(new() {
+            Commands = new[] { new Command { Id = "c", Parameters = new[] { CreateEndpointParameterMeta("p") } } }
+        });
+        Assert.True(resolver.TryResolve(new("c", new[] { new Parsing.Parameter("p", new[] { new PlainText("s.l") }) }), out var script, out var label));
+        Assert.Equal("s", script);
+        Assert.Equal("l", label);
+    }
+
+    private Parameter CreateEndpointParameterMeta (string id)
+    {
+        return new Parameter {
+            Id = id,
+            ValueType = ValueType.String,
+            ValueContainerType = ValueContainerType.Named,
+            ValueContext = new ValueContext[] {
+                new() { Type = ValueContextType.Resource, SubType = "Scripts" },
+                new() { Type = ValueContextType.Constant, SubType = "Labels/{:Path[0]??$Script}" }
+            }
+        };
+    }
+}
---
+++ backend/Naninovel.Common/Metadata/EndpointResolver.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Naninovel.Parsing;
+
+namespace Naninovel.Metadata;
+
+/// <summary>
+/// Allows resolving navigation endpoint (script name and/or label) from parsed commands.
+/// </summary>
+public class EndpointResolver
+{
+    private const string labelExpression = "Labels/{:Path[0]??$Script}";
+    private readonly NamedValueParser namedParser = new();
+    private readonly MetadataProvider provider;
+
+    public EndpointResolver (MetadataProvider provider)
+    {
+        this.provider = provider;
+    }
+
+    /// <summary>
+    /// When specified command has a parameter with navigation context (script name and/or label),
+    /// returns true and assigns related out arguments; returns false otherwise.
+    /// </summary>
+    /// <param name="command">Command to extract the endpoint from.</param>
+    /// <param name="script">When found, assigns script name to the argument; null otherwise.</param>
+    /// <param name="label">When found, assigns label to the argument; null otherwise.</param>
+    /// <returns>Whether script name and/or label were found in one of the command parameters.</returns>
+    public bool TryResolve (Parsing.Command command, out string? script, out string? label)
+    {
+        script = null;
+        label = null;
+
+        foreach (var parameter in command.Parameters)
+            if (IsScriptAndLabelValue(parameter))
+            {
+                (script, label) = namedParser.Parse(parameter.Value.ToString());
+                return true;
+            }
+
+        return false;
+
+        bool IsScriptAndLabelValue (Parsing.Parameter parameter) =>
+            provider.FindParameter(command.Identifier.Text, parameter.Identifier?.Text ?? "")
+                ?.ValueContext?.ElementAtOrDefault(1)?.SubType == labelExpression;
+    }
+}
