ae89c0a2c91b65fef7978eac10f4531f72e20181
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-05-23 20:41:54 +0300

    add debug

---
+++ backend/Naninovel.Common.Modern/Debug.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Naninovel;
+
+public class Debug
+{
+    private static ILogger? logger;
+
+    public Debug (ILogger logger)
+    {
+        Debug.logger = logger;
+    }
+
+    public static void Log (string message)
+    {
+        logger?.Info(message);
+    }
+
+    public static void Trace (string? message = null)
+    {
+        Log($"{message ?? "Trace"}: {Environment.StackTrace}");
+    }
+}
---
+++ backend/Naninovel.Common.Test/DebugTest.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using Xunit;
+
+namespace Naninovel.Test;
+
+public class DebugTest
+{
+    private readonly Mock<ILogger> logger = new();
+
+    [Fact]
+    public void WhenNotConstructedDoesntLog ()
+    {
+        Debug.Log("foo");
+        Debug.Trace();
+        logger.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public void LogsInfoWhenConstructed ()
+    {
+        _ = new Debug(logger.Object);
+        Debug.Log("foo");
+        Debug.Trace("bar");
+        logger.Verify(l => l.Info("foo"));
+        logger.Verify(l => l.Info(It.Is<string>(s => s.Contains("bar"))));
+    }
+
+    [Fact]
+    public void WhenTracingWithoutMessagePrependsTrace ()
+    {
+        _ = new Debug(logger.Object);
+        Debug.Trace();
+        logger.Verify(l => l.Info(It.Is<string>(s => s.Contains("Trace"))));
+    }
+}
