17d32aba2c9bd30d602f7768d0e55f97aa621599
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-07-28 19:57:58 +0300

    add managed text detector

---
+++ backend/Naninovel.Common.Test/ManagedText/ManagedTextDetectorTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Naninovel.ManagedText.Test;
+
+public class ManagedTextDetectorTest
+{
+    public static IEnumerable<object[]> Facts { get; } = new[] {
+        IsInline(""),
+        IsInline("foo: bar"),
+        IsInline("; comment"),
+        IsInline(" # space before id"),
+        IsMultiline("# id"),
+        IsMultiline("; comment\n# id\nfoo bar baz"),
+        IsMultiline("# foo\nauthor: text"),
+        IsMultiline("author: text\n# id")
+    };
+
+    [Theory, MemberData(nameof(Facts))]
+    public void ParseTheory (string text, bool isMultiline)
+    {
+        Assert.Equal(isMultiline, ManagedTextDetector.IsMultiline(text));
+    }
+
+    private static object[] IsMultiline (string text)
+    {
+        return new object[] { text, true };
+    }
+
+    private static object[] IsInline (string text)
+    {
+        return new object[] { text, false };
+    }
+}
---
+++ backend/Naninovel.Common/ManagedText/ManagedTextDetector.cs
@@ -0,0 +1,19 @@
+﻿using Naninovel.Utilities;
+using static Naninovel.ManagedText.ManagedTextConstants;
+
+namespace Naninovel.ManagedText;
+
+/// <summary>
+/// Allows checking whether provided serialized managed text document string
+/// is in multiline or inline format.
+/// </summary>
+public static class ManagedTextDetector
+{
+    public static bool IsMultiline (string text)
+    {
+        foreach (var line in text.IterateLines())
+            if (line.StartsWithOrdinal(RecordMultilineKeyLiteral))
+                return true;
+        return false;
+    }
+}
