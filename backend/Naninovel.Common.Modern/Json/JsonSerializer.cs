6228461042749b4dbe430d34c51d457f9b0a4f95
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-10-18 01:55:15 +0300

    etc

--- backend/Naninovel.Common.Modern/Json/JsonSerializer.cs
+++ backend/Naninovel.Common.Modern/Json/JsonSerializer.cs
@@ -4,25 +4,25 @@ namespace Naninovel;
 
 public class JsonSerializer : ISerializer
 {
-    private readonly JsonSerializerOptions options;
+    public JsonSerializerOptions Options { get; }
 
     public JsonSerializer (JsonSerializerOptions? options = null)
     {
-        this.options = options ?? new();
-        this.options.TypeInfoResolverChain.Add(JsonContext.Default);
+        Options = options ?? new();
+        Options.TypeInfoResolverChain.Add(JsonContext.Default);
     }
 
     public string Serialize (object poco)
     {
         var type = poco.GetType();
-        if (!options.TryGetTypeInfo(type, out var info))
+        if (!Options.TryGetTypeInfo(type, out var info))
             throw new SerializeError($"Failed to serialize '{type.FullName}' object: missing type info.");
         return System.Text.Json.JsonSerializer.Serialize(poco, info);
     }
 
     public object Deserialize (string serialized, Type type)
     {
-        if (!options.TryGetTypeInfo(type, out var info))
+        if (!Options.TryGetTypeInfo(type, out var info))
             throw new SerializeError($"Failed to deserialize '{type.FullName}' object: missing type info.");
         return System.Text.Json.JsonSerializer.Deserialize(serialized, info) ??
                throw new SerializeError($"Failed to deserialize '{serialized}' JSON to '{type.FullName}' object: result is null.");
