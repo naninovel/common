ed0c37beb1f5b1a202be5bd8fba4f49fcc1a7303
Artyom Sovetnikov <2056864+elringus@users.noreply.github.com>
2024-01-24 03:04:50 +0300

    brushing (#7)

--- src/metadata.ts
+++ src/metadata.ts
@@ -6,30 +6,30 @@ export function getDefaultMetadata(): Metadata.Project {
 }
 
 export function mergeMetadata(...metadata: Metadata.Project[]) {
-    const mergedMetadata: Metadata.Project = {} as any;
+    const mergedMetadata: Metadata.Project = <never>{};
     for (const meta of metadata)
-        mergeObject(deepClone(meta), mergedMetadata);
+        mergeObject(deepClone(meta), <never>mergedMetadata);
     return mergedMetadata;
 }
 
-function deepClone(object: any) {
+function deepClone(object: object) {
     return JSON.parse(JSON.stringify(object));
 }
 
-function mergeObject(source: any, destination: any) {
+function mergeObject(source: Record<string, unknown>, destination: Record<string, unknown>) {
     for (const key in source)
         if (source.hasOwnProperty(key))
             mergeKey(key, source[key], destination);
 }
 
-function mergeKey(key: string, value: any, destination: any) {
+function mergeKey(key: string, value: unknown, destination: Record<string, unknown>) {
     if (!destination.hasOwnProperty(key))
         destination[key] = value;
     else if (Array.isArray(value))
-        if (key === "commands") destination[key] = mergeCommands(value, destination[key]);
-        else destination[key] = destination[key].concat(value);
+        if (key === "commands") destination[key] = mergeCommands(value, <Array<Metadata.Command>>destination[key]);
+        else destination[key] = (<unknown[]>destination[key]).concat(value);
     else if (typeof value === "object")
-        mergeObject(value, destination[key]);
+        mergeObject(<never>value, <never>destination[key]);
 }
 
 function mergeCommands(custom: Array<Metadata.Command>, builtin: Array<Metadata.Command>) {
