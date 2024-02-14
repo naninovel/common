b0e07b55e2b300a33d90e7f1bdad7b1a1dfceadf
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-02-14 19:31:06 +0300

    etc

--- backend/Naninovel.Common.Test/ManagedText/MultilineManagedTextSerializerTest.cs
+++ backend/Naninovel.Common.Test/ManagedText/MultilineManagedTextSerializerTest.cs
@@ -10,7 +10,6 @@ public class MultilineManagedTextSerializerTest
         Fact("; header\n\n# key\n; comment\nvalue\n", "header", 1, [new("key", "value", "comment")]),
         Fact("# key\n<br>\n<br>\n\n", null, 0, [new("key", "<br><br>")]),
         Fact("\n# key1\n<br>\n<br>\n\n\n# key2\n; comment\n\n", null, 1, [new("key1", "<br><br>"), new("key2", "", "comment")]),
-
         Fact("# k1|k2\n; c1|c2\nv1|v2\n", "", 0, [new("k1", "v1", "c1"), new("k2", "v2", "c2")], [new(0, 1)]),
         Fact("# k1|k2\nv1|v2\n", "", 0, [new("k1", "v1"), new("k2", "v2")], [new(0, 1)]),
         Fact("# k1|k2\n|\n# k3\n; c3\n\n", "", 0, [new("k1", ""), new("k2", ""), new("k3", "", "c3")], [new(0, 1)]),
