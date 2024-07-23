b731ea9bd0ddb44370f112d2225d4c37950ee740
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-07-23 20:19:45 +0300

    add path utils

---
+++ backend/Naninovel.Common.Test/Utilities/PathUtilitiesTest.cs
@@ -0,0 +1,17 @@
+using static Naninovel.PathUtilities;
+
+namespace Naninovel.Utilities.Test;
+
+public class PathUtilitiesTest
+{
+    [Fact]
+    public void CanResolveScriptName ()
+    {
+        Assert.Equal("FOO", ResolveScriptName(@"C:\DIR\FOO.NANI"));
+        Assert.Equal("foo", ResolveScriptName("/dir/foo.nani"));
+        Assert.Equal("foo", ResolveScriptName("ws://foo.nani"));
+        Assert.Equal("foo", ResolveScriptName("foo.nani"));
+        Assert.Equal("foo", ResolveScriptName("foo"));
+        Assert.Equal("foo.bar", ResolveScriptName("foo.bar.nani"));
+    }
+}
