429b099e349aaa35595e6d6b6cdc768945f2cb65
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-08-26 18:53:22 +0300

    chore: docs

--- backend/Naninovel.Common/ObjectPool/PooledObject.cs
+++ backend/Naninovel.Common/ObjectPool/PooledObject.cs
@@ -3,6 +3,10 @@ namespace Naninovel;
 /// <summary>
 /// A disposable wrapper over a pooled object.
 /// </summary>
+/// <remarks>
+/// .NET has optimization that prevents boxing disposable structs:
+/// https://github.com/dotnet/csharplang/discussions/8337.
+/// </remarks>
 public readonly struct PooledObject<T> (T obj, ObjectPool<T> pool)
     : IDisposable where T : class
 {
